34512155c3cc31ea485786c1aad796227140905a
Pawel Sawicki <public@psawicki.de>
2021-11-05 11:21:45 +0100

    Adding a Books Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
-import { BooksController } from './books/books.controller';
 
-import { HttpModule } from '@nestjs/axios';
-import { BooksService } from './books/books.service';
-import { HttpBooksService } from './books/http-books.service';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [HttpModule],
-  controllers: [AppController, CatsController, BooksController],
-  providers: [
-    AppService,
-    {
-      provide: BooksService,
-      useClass: HttpBooksService,
-    },
-  ],
+  imports: [BooksModule],
+  controllers: [AppController, CatsController],
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/books/books.module.ts
@@ -0,0 +1,18 @@
+import { HttpModule } from '@nestjs/axios';
+import { Module } from '@nestjs/common';
+import { BooksController } from './books.controller';
+import { BooksService } from './books.service';
+import { HttpBooksService } from './http-books.service';
+
+@Module({
+  controllers: [BooksController],
+  providers: [
+    {
+      provide: BooksService,
+      useClass: HttpBooksService,
+    },
+  ],
+  imports: [HttpModule],
+  exports: [],
+})
+export class BooksModule {}
